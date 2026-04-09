04b2cb283438e55140606abfc91965d417aeead9
FrutasAbocados <frutasabocados@gmail.com>
2026-04-10 00:59:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 // 3. Copia "Project URL" y "anon public" key
 // ============================================================
 
-window.SUPABASE_URL = https://ucjkyjhvvdofyaizzdbk.supabase.co;
-window.SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjamt5amh2dmRvZnlhaXp6ZGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NjA4MTgsImV4cCI6MjA5MTMzNjgxOH0.z9xqBFxqupXVDABnyTpCjuJrZQP4OlbLxvVT-6BD8pI;
+window.SUPABASE_URL = 'https://ucjkyjhvvdofyaizzdbk.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjamt5amh2dmRvZnlhaXp6ZGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NjA4MTgsImV4cCI6MjA5MTMzNjgxOH0.z9xqBFxqupXVDABnyTpCjuJrZQP4OlbLxvVT-6BD8pI';
